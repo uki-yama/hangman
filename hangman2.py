fd5b3fa6c7a65a1ef8f40af5dec7a97042e0ca79
uki-yama <xxx89yvyvyv567@gmail.com>
2020-12-02 17:48:49 +0900

    my first commit

---
+++ hangman2.py
@@ -0,0 +1,41 @@
+def hangman():
+        words = [ "dog", "cat", "rabbit"]
+        last_number = len(words)
+        import random
+        random_number = random.randint(0,int(last_number)-1)
+        word = words[int(random_number)]
+        wrong = 0
+        stages = ["",
+		  "_____     ",
+		  "|         ",
+		  "|    |    ",
+		  "|    0    ",
+		  "|   /|    ",
+		  "|   /     ",
+		  "|         ",
+		  ]
+        rletters = list(word)
+        board = ["_"]*len(word)
+        win = False
+        print("Wellcome to hangman!!")
+        while wrong < len(stages) - 1:
+                print("\n")
+                msg = "predict one word"
+                char = input(msg)
+                if char in rletters:
+                        cind = rletters.index(char)
+                        board[cind] = char
+                        rletters[cind] = "$"
+                else:
+                        wrong += 1
+                print(" ".join(board))
+                e = wrong + 1
+                print("\n".join(stages[0:e]))
+                if "_" not in board:
+                        print("You win!")
+                        print(" ".join(board))
+                        win = True
+                        break
+        if not win:
+                        print("\n".join(stages[0:wrong+1]))
+                        print("You lose. The correct answer is {}.".format(word))
